1600ca150f663c6762e1650738d337ff07ee1628
gorohovIlya <ilya.gorohov.404@gmail.com>
2026-05-16 10:00:03 +0800

    Added basic routes for reviews

---
+++ routes/reviews.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\ProcessReviewController;
+use App\Http\Controllers\ReviewController;
+use Illuminate\Support\Facades\Route;
+
+Route::controller(ReviewController::class)->group(function() 
+{
+    Route::get('/', 'index');
+    Route::get('/create', 'create');
+    Route::get('/{reviewId}', 'show');
+    Route::post('/', 'store');
+});
+
+Route::get('/{reviewId}/process', ProcessReviewController::class);
