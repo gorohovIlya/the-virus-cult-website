324a9d7026beca36430e1082f8b2aba4f951ead4
gorohovIlya <ilya.gorohov.404@gmail.com>
2026-05-16 10:10:45 +0800

    Removed redundant line

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,24 @@
 <?php
 
+use App\Http\Middleware\AssignRequestId;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function() {
+            Route::prefix('reviews')
+                ->name('reviews.')
+                ->group(base_path('routes/reviews.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // $middleware->prepend(AssignRequestId::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
