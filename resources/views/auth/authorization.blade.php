bfe991f8f664dadb071fe2367da312fa4c6252ad
gorohovIlya <ilya.gorohov.404@gmail.com>
2026-05-16 23:48:26 +0800

    Edited authorization view and added CSS for it

--- resources/views/auth/authorization.blade.php
+++ resources/views/auth/authorization.blade.php
@@ -4,73 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Virus Cult - Visual Novel</title>
-    @if (file_exists('resources/css/authorization.css'))
-        @vite(['resources/css/authorization.css'])
-    @else
-        <style>        
-            body{
-                background: black;
-                margin: 0;
-                padding: 0;
-            }
-
-            form{
-                display: flex;
-                justify-content: center;
-                flex-direction: column;
-                width: 320px; 
-                margin-left: auto;
-                margin-right: auto;
-                gap: 12px;
-                padding: 20px;
-            }
-
-            h1{
-                color: white;
-                text-align: center;
-                font-size: 48px;
-            }
-
-            label{
-                color: white;
-            }
-
-            form > div {
-                display: flex;
-                flex-direction: column;
-                gap: 4px;
-                width: 100%;
-            }
-
-            input, button {
-                padding: 6px 8px;
-                font-size: 1rem;
-                color: white;
-                background-color: #1a1a2e;
-                border: #2b044b solid 3px;
-                border-radius: 50px;
-                width: 100%;
-                box-sizing: border-box;
-            }
-
-            button{
-                transition: all 0.3s ease;
-                cursor: pointer;
-                margin-top: 20px;
-            } 
-
-            button:hover {
-                background: linear-gradient(135deg, #4a2d77, #2a1a4a);
-                border-color: #b77cff;
-                transform: translateY(-3px);
-                box-shadow: 0 8px 20px rgba(128, 0, 128, 0.4);
-            }
-                
-            button:active {
-                transform: translateY(1px);
-            }
-        </style>
-    @endif
+    @vite(['resources/css/authorization.css'])
 </head>
 <body>
     <h1>Authorization</h1>
