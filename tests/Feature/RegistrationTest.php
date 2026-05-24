ded337d216fedda365f6242d0d7840b3f14ecfd3
gorohovIlya <ilya.gorohov.404@gmail.com>
2026-05-24 10:31:38 +0800

    feature: added feature-test for registration

---
+++ tests/Feature/RegistrationTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+use Tests\TestCase;
+use App\Models\User;
+
+class RegistrationTest extends TestCase
+{
+    public function test_can_register() : void
+    {
+        $response = $this->post(
+            '/register',
+            [
+                'name' => 'testuser',
+                'email' => 'testuser@gmail.com',
+                'password' => 'testuser_ABC_123',
+                'password_confirmation' => 'testuser_ABC_123',
+                'newsletter' => false
+            ]
+        );
+
+        $this->assertDatabaseHas('users', ['email' => 'testuser@gmail.com']);
+
+        $response->assertRedirect('/');
+        $this->assertAuthenticated();
+    }
+
+    public function test_can_register_with_existing() : void
+    {
+        User::factory()->create(['email' => 'existing@mail.com']);
+
+        $response = $this->post('/register',
+            [
+                'name' => 'UserWithExistingEmail',
+                'email' => 'existing@mail.com',
+                'password' => 'testuser2_ABC_123',
+                'password_confirmation' => 'testuser2_ABC_123',
+                'newsletter' => true
+            ]
+        );
+
+        $response->assertSessionHasErrors('email');
+    }
+}
