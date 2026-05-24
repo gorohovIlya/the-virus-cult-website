b84d55057380d22b8ee9b3834b78db3f3e715792
gorohovIlya <ilya.gorohov.404@gmail.com>
2026-05-24 11:19:19 +0800

    test: added page access test

---
+++ tests/Feature/PageAccessTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PageAccessTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_pages_access() : void
+    {
+        $pages = ['/', '/download', '/feedback', '/support-us', '/about-us'];
+        foreach($pages as $page)
+        {
+            $response = $this->get($page);
+            $response->assertStatus(200);
+        }
+    }
+}   
